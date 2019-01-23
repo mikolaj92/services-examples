19748acee1b06e4c3501c46d2f91e32248cc6788
Patryk Mikolajczyk <p.mikolajczyk@bsgroup.eu>
2019-01-23 10:38:54 +0100

    Initial Commit

---
+++ NetworkingTests/NetworkingTests.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingTests.h
+//  NetworkingTests
+//
+//  Created by Patryk Mikolajczyk on 1/23/19.
+//  Copyright © 2019 Patryk Mikolajczyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkingTests.
+FOUNDATION_EXPORT double NetworkingTestsVersionNumber;
+
+//! Project version string for NetworkingTests.
+FOUNDATION_EXPORT const unsigned char NetworkingTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingTests/PublicHeader.h>
+
+
